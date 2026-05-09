b917fc0de74571d7a84fb078a525e32238842cb5
nying <71574223+cehune@users.noreply.github.com>
2026-05-08 22:25:37 -0400

    Add Ray weights into sampling and Iteration structure and Segment Pooling (#10)

--- segments/src/sampler.py
+++ segments/src/sampler.py
@@ -5,14 +5,15 @@ def luminance_rgb(c: mi.Color3f) -> float:
     """Perceived luminance weights for RGB."""
     return 0.2126 * c[0] + 0.7152 * c[1] + 0.0722 * c[2]
 
-def generate_path(scene, sampler, starting_weight, starting_sp, si, is_camera_path = True, max_depth=16, rr_start_depth = 3):
+def generate_path(scene, sampler, starting_weight,starting_sp, si, technique = SegmentTechnique.CAMERA, max_depth=16, rr_start_depth = 3):
     """
     starting sp is either the camera or light
     si is the first surface interaction
+
+    returns a path of disconnected segments with independant throughput from x to y
     """
     first_sp = SurfacePoint(si = si)
     segment_path = []
-    weight = starting_weight
 
     # if it's a light, then we just return after setting the first hit to a light src
     emitter = si.emitter(scene)
@@ -20,7 +21,7 @@ def generate_path(scene, sampler, starting_weight, starting_sp, si, is_camera_pa
         first_sp.is_light = True
         first_sp.Le = emitter.eval(si)
 
-    segment_path.append(Segment(starting_sp, first_sp, throughput=mi.Color3f(weight), is_camera_path=is_camera_path))
+    segment_path.append(Segment(starting_sp, first_sp, throughput=mi.Color3f(starting_weight), technique=technique))
 
     if first_sp.is_light:
         return segment_path
@@ -40,14 +41,16 @@ def generate_path(scene, sampler, starting_weight, starting_sp, si, is_camera_pa
         if bs.pdf == 0 or dr.all(bsdf_weight == 0):
             break
 
-        weight *= bsdf_weight
+        # in standard path tracing we would normally just modify weight absed on bsdf weight and carry forward
+        # since segments are disocnnected, only use a local weight
+        local_weight = bsdf_weight
 
         # russian roulette
         if i >= rr_start_depth:
-            q = min(0.97, luminance_rgb(weight))
+            q = min(0.97, luminance_rgb(local_weight))
             if sampler.next_1d() >= q:
                 break
-            weight /= q
+            local_weight /= q
 
         # bs.wo is the sample direction outgoing in local space, convert to world
         world_wo = si.to_world(bs.wo)
@@ -62,15 +65,13 @@ def generate_path(scene, sampler, starting_weight, starting_sp, si, is_camera_pa
         if emitter_hit:
             curr_sp.is_light = True
             curr_sp.Le = emitter_hit.eval(si_next)
-            seg = Segment(prev_sp, curr_sp, throughput=mi.Color3f(weight), is_camera_path=is_camera_path)
-            segment_path.append(seg)
-            break
 
-        if dr.norm(curr_sp.p - prev_sp.p) < 1e-4:
-            break
+        segment_path.append(
+            Segment(prev_sp, curr_sp, throughput=mi.Color3f(local_weight), technique=technique)
+        )
 
-        seg = Segment(prev_sp, curr_sp, throughput=mi.Color3f(weight))
-        segment_path.append(seg)
+        if curr_sp.is_light or dr.norm(curr_sp.p - prev_sp.p) < 1e-4: # same spot basically
+            break
 
         prev_sp = curr_sp
         si = si_next
@@ -81,7 +82,6 @@ def sample_light_path(scene, sampler, max_depth=16, rr_start_depth=3):
     # just get an emitter, weighted by their power
     emitter_sample = scene.sample_emitter(sampler.next_1d())
     emitter = emitter = scene.emitters()[emitter_sample[0]]
-    print(type(emitter_sample[0]))
     emitter_pdf = emitter_sample[1] # probability of having selected that emitter
     # dividing by this pdf later is importance sampling, want to sample from bright lights
     # more often
@@ -117,20 +117,21 @@ def sample_light_path(scene, sampler, max_depth=16, rr_start_depth=3):
     # generate weight cost theta / pdfs
     weight = emitted_ray_weight / emitter_pdf
     
-    return generate_path(scene, sampler, weight, light_sp, si, is_camera_path=False, max_depth=max_depth, rr_start_depth=rr_start_depth)
+    return generate_path(scene, sampler, weight, light_sp, si, 
+                         technique=SegmentTechnique.LIGHT, max_depth=max_depth, 
+                         rr_start_depth=rr_start_depth)
 
-def sample_camera_path(scene, sampler, ray, max_depth=16, rr_start_depth=3):
-    # TODO: Change to BDPT which means adding ray weight into here, not applied after 
-    # in the main render loop 
-    si = scene.ray_intersect(ray)
+def sample_camera_path(scene, sampler, ray, ray_weight, si, max_depth=16, rr_start_depth=3):
     if not si.is_valid():
         return []
     
-    # first segment: view origin -> first hit
-    cam_sp = SurfacePoint(p = ray.o, n = -ray.d)
+    cam_sp = SurfacePoint(p=ray.o, n=mi.Vector3f(-ray.d))
     cam_sp.is_camera = True
 
-    return generate_path(scene, sampler, mi.Color3f(1.0), cam_sp, si, max_depth, rr_start_depth)
+    return generate_path(scene, sampler, ray_weight, cam_sp, si,
+                          technique=SegmentTechnique.CAMERA,
+                          max_depth=max_depth,
+                          rr_start_depth=rr_start_depth)
 
 
 def evaluate_path_base(segments: list[Segment]) -> mi.Color3f:
